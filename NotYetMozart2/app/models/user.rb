018fa64d7a3d090fd638daf406f6ed5972181e4e
Clemens Kazda <clemkaz@Clemenss-MacBook-Pro.local>
2014-02-04 19:12:57 +0000

    Implement Authentication (works)

--- NotYetMozart2/app/models/user.rb
+++ NotYetMozart2/app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :background_image, :created_at, :email, :name, :password_digest, :role, :user_image
+  has_secure_password
+
+  attr_accessible :background_image, :created_at, :email, :name, :password, :password_confirmation, :role, :user_image
+
   mount_uploader :user_image, UserImageUploader
   mount_uploader :background_image, BackgroundImageUploader
+
+  validates :password, presence: true, on: :created
+  validates :email, presence: true
+  validates :email, uniqueness: true
 
 end
--- NotYetMozart2/config/routes.rb
+++ NotYetMozart2/config/routes.rb
@@ -1,6 +1,16 @@
 NotYetMozart2::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   resources :songs
   resources :users
+
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy', as: :logout
+  resources :sessions
 
   root :to => 'home#index'
 end
@@ -18,13 +28,23 @@ end
 #           PUT    /users/:id(.:format)      users#update
 #           DELETE /users/:id(.:format)      users#destroy
 
-# songs GET    /songs(.:format)          songs#index
+#     songs GET    /songs(.:format)          songs#index
 #           POST   /songs(.:format)          songs#create
 #  new_song GET    /songs/new(.:format)      songs#new
 # edit_song GET    /songs/:id/edit(.:format) songs#edit
 #      song GET    /songs/:id(.:format)      songs#show
 #           PUT    /songs/:id(.:format)      songs#update
 #           DELETE /songs/:id(.:format)      songs#destroy
+
+#        login GET    /login(.:format)             sessions#new
+#       logout GET    /logout(.:format)            sessions#destroy
+#     sessions GET    /sessions(.:format)          sessions#index
+#              POST   /sessions(.:format)          sessions#create
+#  new_session GET    /sessions/new(.:format)      sessions#new
+# edit_session GET    /sessions/:id/edit(.:format) sessions#edit
+#      session GET    /sessions/:id(.:format)      sessions#show
+#              PUT    /sessions/:id(.:format)      sessions#update
+#              DELETE /sessions/:id(.:format)      sessions#destroy
 
 
 
